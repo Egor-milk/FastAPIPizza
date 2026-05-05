732025ee5a32897c2cb9253e35e7f2a49edd9720
Egor-milk <mrpixelegor@gmail.com>
2026-05-05 15:54:47 +0700

    все работает, не трогать

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from config import settings
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:48erons2445@localhost:5432/postgres")
+DATABASE_URL = settings.DB_URL
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
